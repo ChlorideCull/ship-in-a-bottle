b3530ebba2adfec32df608d46fb85703fb0e7fff
Chloride Cull <chloride@devurandom.net>
2014-03-16 16:55:25 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ship-in-a-bottle',
+        version='0.1',
+        description='Run multiple WSGI scripts and/or bottles at the same port.',
+        author='Chloride Cull',
+        author_email='chloride@devurandom.net',
+        url='http://devurandom.net/python/',
+        scripts=['ship-in-a-bottle.py'],
+        data_files=[('/etc', ['cfgs/ship-in-a-bottle.conf']),
+                    ('/srv/bottles', ['cfgs/ships.conf', 'examples/hello.py'])],
+        classifiers=["Development Status :: 4 - Beta",
+                     "Environment :: Web Environment",
+                     "Intended Audience :: System Administrators",
+                     "Framework :: Bottle",
+                     "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+                     "Programming Language :: Python :: 3"]
+     )
